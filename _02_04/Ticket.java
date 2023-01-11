9b56e2368fe132a354005cb7e2d504ece75cdb1d
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-01-11 23:39:42 +0800

    creathing methods

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -10,10 +10,32 @@ public class Ticket {
   private double price;
   private boolean isReturn;
 
+
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public String setDestination(){
+    return destination;
+  }
+  public double setPrice(){
+    return price;
+  }
 
-  // Add a separate method to get the value of each field, called getDestination,
+  public boolean setIsReturn(){
+    return isReturn;
+  }
+
+
+    // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public void setDestination(String destination){
+    this.destination = destination;
+  }
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setIsReturn(boolean isReturn){
+    this.isReturn = isReturn;
+  }
+
 
 }
