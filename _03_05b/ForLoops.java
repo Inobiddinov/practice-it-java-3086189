510753d444066751c7d10a80d2087db3f0d05c8e
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-01-12 00:10:02 +0800

    for loop exercise

--- _03_05b/ForLoops.java
+++ _03_05b/ForLoops.java
@@ -4,10 +4,20 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
+    String love = "I love for Loops";
+    for (int a=0; a<5; a++){
+      System.out.println(love);
+    }
     // Write a for loop that prints out the numbers 1 to 10
-
+    System.out.println();
+    for(int i =1; i<=10; i++){
+      System.out.println(i);
+    }
+    System.out.println();
     // Write a for loop that prints out the numbers 10 to 1
+    for(int i =10; i>0; i--){
+      System.out.println(i);
+    }
 
   }
 
